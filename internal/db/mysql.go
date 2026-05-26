43546da5f693bac1738e8ab95e44c356d1edddff
yehuoshun <2431038086@qq.com>
2026-05-26 18:58:02 +0800

    修改数据库配置

--- internal/db/mysql.go
+++ internal/db/mysql.go
@@ -22,8 +22,8 @@ type Config struct {
 
 func DefaultConfig() Config {
 	return Config{
-		User:     "zangye",
-		Password: "zangye123",
+		User:     "root",
+		Password: "root",
 		Host:     "127.0.0.1",
 		Port:     3306,
 		DBName:   "zang_ye",
