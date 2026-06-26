c645e5cd7a3b9afab3bc83718f4f5d5171f6472c
yehuoshun <69134831+yehuoshun@users.noreply.github.com>
2026-06-26 18:17:06 +0800

    fix: 修复 import 路径

--- frontend/src/app/main.ts
+++ frontend/src/app/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import './styles/global.css'
+import router from '../router'
+import '../styles/global.css'
 
 const app = createApp(App)
 app.use(router)
